101777c4bcd170b2df3c0274fab77fead34ddee3
Akiyoshi-Yagi <akiyoshimerry@yahoo.co.jp>
2020-03-08 00:07:46 +0900

    playerの動きのanimationの実装

--- Assets/Scripts/playerMove.cs
+++ Assets/Scripts/playerMove.cs
@@ -9,11 +9,13 @@ public class playerMove : MonoBehaviour
     //コンポーネントもフィールドに入れられる
     private CharacterController characterController;
     private Vector3 velocity;
+    private Animator animator;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
         transform.position = new Vector3(0f, 1f, 0f);
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,7 +23,18 @@ public class playerMove : MonoBehaviour
     {
         if (characterController.isGrounded)
         {
+            //magnitudeはベクトルの大きさ
             velocity = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            if(velocity.magnitude > 0.1f)
+            {
+                animator.SetFloat("Speed", velocity.magnitude);
+                //キャラクターの向きも押した方にする！
+                transform.LookAt(transform.position + velocity);
+            }
+            else
+            {
+                animator.SetFloat("Speed", 0f);
+            }
         }
         velocity.y += Physics.gravity.y * Time.deltaTime;
         characterController.Move(velocity * walkSpeed * Time.deltaTime);
