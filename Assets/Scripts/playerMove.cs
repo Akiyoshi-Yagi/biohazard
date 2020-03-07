9306430221b61f792a6da3f3e01ed052ce1af5a0
Akiyoshi-Yagi <akiyoshimerry@yahoo.co.jp>
2020-03-07 23:40:28 +0900

    プレイヤーの前後左右動きの実装

---
+++ Assets/Scripts/playerMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed;
+
+    //コンポーネントもフィールドに入れられる
+    private CharacterController characterController;
+    private Vector3 velocity;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        transform.position = new Vector3(0f, 1f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            velocity = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        }
+        velocity.y += Physics.gravity.y * Time.deltaTime;
+        characterController.Move(velocity * walkSpeed * Time.deltaTime);
+
+    }
+}
