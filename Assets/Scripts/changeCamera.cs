f12559b5b88c00ffa15efc48bad24d903c8a2932
Akiyoshi-Yagi <akiyoshimerry@yahoo.co.jp>
2020-03-08 12:03:23 +0900

    懐中電動の実装

---
+++ Assets/Scripts/changeCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeCamera : MonoBehaviour
+{
+    [SerializeField] GameObject mainCamera;
+    [SerializeField] GameObject otherCamera;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            //GameObject自身がactiveがどうかを調べるプロパティとしてactiveSelf
+            mainCamera.SetActive(!mainCamera.activeSelf);
+            otherCamera.SetActive(!otherCamera.activeSelf);
+        }
+    }
+}
