febc9e8410a2a9a370f846ad313d6cd6b3179936
Akiyoshi-Yagi <akiyoshimerry@yahoo.co.jp>
2020-03-10 13:53:59 +0900

    敵キャラをある場所から自動でランダムに量産する仕様を実装

---
+++ Assets/Scripts/AppearEnemys.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearEnemys : MonoBehaviour
+{
+    [SerializeField] GameObject[] enemys;
+    [SerializeField] float appearNextTime;
+    [SerializeField] int maxNumOfEnemys;
+
+    private float elapsedTime;
+    private int numberOfEnemys;
+
+    void Start()
+    {
+        numberOfEnemys = 0;
+        elapsedTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (numberOfEnemys >= maxNumOfEnemys)
+        {
+            return;
+        }
+
+        elapsedTime += Time.deltaTime;
+
+        if(elapsedTime > appearNextTime)
+        {
+            elapsedTime = 0f;
+            GenerateEnemy();
+        }
+
+    }
+
+    void GenerateEnemy()
+    {
+        var randomValue = Random.Range(0, enemys.Length);
+        var randomRotationY = Random.value * 360f;
+
+        GameObject.Instantiate(enemys[randomValue], transform.position, Quaternion.Euler(0f, randomRotationY, 0f));
+
+        numberOfEnemys++;
+        elapsedTime = 0;
+    }
+}
