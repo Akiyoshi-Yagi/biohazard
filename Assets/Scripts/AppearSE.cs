7aa416a7d779a1cb5ddf1c1b16715ba240d063d2
Akiyoshi-Yagi <akiyoshimerry@yahoo.co.jp>
2020-03-11 12:35:39 +0900

    敵キャラクターの出現エフェクトとSEの実装

---
+++ Assets/Scripts/AppearSE.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearSE : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    [SerializeField] private AudioClip appearSE;
+    void Start()
+
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.PlayOneShot(appearSE);
+    }
+
+    
+}
